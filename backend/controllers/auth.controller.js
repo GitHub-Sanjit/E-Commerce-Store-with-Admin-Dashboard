15085c52bf4acd988ffa91184a6f37af3b37f257
sanjit <sanjit.est14@gmail.com>
2026-06-16 00:25:56 +0600

    Backend Setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send("Signup route Called");
+};
+
+export const login = async (req, res) => {
+  res.send("Login route Called");
+};
+
+export const logout = async (req, res) => {
+  res.send("Logout route Called");
+};
