243e9d3d98f667a4716cb3679a0fc48638bb132c
sanjit <sanjit.est14@gmail.com>
2026-06-16 11:56:54 +0600

    Logout and Login completed for backend

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 import User from "../models/user.model.js";
-import { redis } from './../lib/redis.js';
+import { redis } from "./../lib/redis.js";
 
 const generateTokens = (userId) => {
   const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
@@ -47,7 +47,7 @@ export const signup = async (req, res) => {
     if (userExists) {
       res.status(400).json({ message: "User Already Exists" });
     }
-    
+
     const user = await User.create({ name, email, password });
 
     // authenticate
@@ -71,9 +71,56 @@ export const signup = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.send("Login route Called");
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user && (await user.comparePassword(password))) {
+      const { accessToken, refreshToken } = generateTokens(user._id);
+
+      await storeRefreshToken(user._id, refreshToken);
+      setCookies(res, accessToken, refreshToken);
+
+      res.status(201).json({
+        user: {
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          role: user.role,
+        },
+        message: "User Logged In Successfully",
+      });
+    } else {
+      res.status(401).json({ message: "Invalid Email or Password" });
+    }
+  } catch (error) {
+    console.log("Error in Log In controller", error.message);
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const logout = async (req, res) => {
-  res.send("Logout route Called");
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    console.log({ refreshToken: refreshToken });
+    if (refreshToken) {
+      const decoded = jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET,
+      );
+
+      const tokenInRedis = await redis.get(`refreshToken:${decoded.userId}`);
+      console.log("Before delete:", tokenInRedis);
+
+      console.log({ decoded: decoded });
+      await redis.del(`refreshToken:${decoded.userId}`);
+    }
+
+    res.clearCookie("accessToken");
+    res.clearCookie("refreshToken");
+    res.json({ message: "Logged Out Successfully!" });
+  } catch (error) {
+    console.log("Error in Log Out controller", error.message);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
 };
